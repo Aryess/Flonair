f93a3503636b371c6c1d35ee8d4056be70bb56d5
Steven Sinatra <ssinatra@freelancer.com>
2016-10-01 11:57:46 +1000

    Don't hardcode the link to socket.io server

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 (() => {
   const background = document.getElementById('background');
 
-  const socket = io('http://localhost:2588');
+  const socket = io('/');
   socket.on('stateChange', channels => {
     background.setAttribute('data-state', channels[1].state.toLowerCase());
   });
